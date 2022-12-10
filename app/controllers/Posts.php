bce9b629f90cae5d830700f33761f973d73710d4
Lou Vang <louvangdev@gmail.com>
2022-12-09 20:13:41 -0600

    Complete edit function for posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -67,6 +67,63 @@
       }
     }
 
+    public function edit($id){
+      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // Sanitize POST array
+        $_POST = filter_input_array(htmlspecialchars(INPUT_POST));
+
+        $data = [
+          'id' => $id,
+          'title' => trim($_POST['title']),
+          'body' => trim($_POST['body']),
+          'user_id' => $_SESSION['user_id'],
+          'title_err' => '',
+          'body_err' => ''
+        ];
+
+        // Validate title
+        if(empty($data['title'])){
+          $data['title_err'] = 'Please enter a title.';
+        }
+
+        // Validate body
+        if(empty($data['body'])){
+          $data['body_err'] = 'Please enter a body.';
+        }
+
+        // Make sure there are no errors
+        if(empty($data['title_err']) && empty($data['body_err'])){
+          if($this->postModel->updatePost($data)){
+            flash('post_message', 'Post Updated');
+            redirect('posts');
+          } else {
+            die('Something went wrong.');
+          }
+        } else {
+          // Load view with errors 
+          $this->view('posts/edit', $data);
+        }
+      } else {
+        // Get existing post from model
+        $post = $this->postModel->getPostById($id);
+        
+        // Check for owner
+        if($post->user_id != $_SESSION['user_id']){
+          redirect('posts');
+        }
+
+        $data = [
+          'id' => $id, 
+          'title' => $post->title,
+          'title_err' => '',
+          'body' => $post->body,
+          'body_err' => ''
+        ];
+
+        $this->view('posts/edit', $data);
+      }
+    }
+
     public function show($id){
       $post = $this->postModel->getPostById($id);
       $user = $this->userModel->getUserById($post->user_id);
---
+++ app/views/posts/edit.php
@@ -0,0 +1,35 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        
+        <div class="title-row">
+          <h1>Edit Post</h1>
+          <a href="<?php echo URLROOT; ?>/posts" class="btn btn-gray"><i class="fa fa-backward"></i> Back</a>
+        </div>
+        
+        <form action="<?php echo URLROOT; ?>/posts/edit/<?php echo $data['id']; ?>" method="post">
+          
+          <div class="add-post-row">
+            <label for="title"><sup>*</sup>Title:</label>
+            <input type="text" name="title" value="<?php echo $data['title']; ?>"/>
+            <span class="error"><?php echo $data['title_err']; ?></span>
+          </div>
+          <div class="add-post-row">
+            <label for="body"><sup>*</sup>Body:</label>
+            <textarea name="body"><?php echo $data['body']; ?></textarea>
+            <span class="error"><?php echo $data['body_err']; ?></span>
+          </div>
+
+          <div class="flex">
+            <div class="col">
+              <button>Add Post</button>
+            </div>
+          </div>
+
+        </form>
+
+      </div>
+    </div>
+  </div>
+<?php require APPROOT.'/views/inc/footer.php'; ?>
